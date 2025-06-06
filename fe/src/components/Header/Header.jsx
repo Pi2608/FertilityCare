d5238053a9802591db34888519d978ae4504ba0b
Pi2608 <ibaxq26@gmail.com>
2025-06-06 23:17:07 +0700

    TreatmentMethod Page + DoctorList

--- fe/src/components/Header/Header.jsx
+++ fe/src/components/Header/Header.jsx
@@ -10,14 +10,14 @@ const Header = () => {
     <header id="header">
         <div className="logo" onClick={() => navigate('/homepage')}>Trung Tâm Hiếm Muộn</div>
         <nav className="nav-links">
-          <a href="/treatment-method">Phương Pháp Điều Trị</a>
-          <a href="#">Đội Ngũ Bác Sĩ</a>
+          <a href="/homepage/treatment-method">Phương Pháp Điều Trị</a>
+          <a href="/homepage/doctor-list">Đội Ngũ Bác Sĩ</a>
           <a href="#">Về Chúng Tôi</a>
           <a href="#">Blog</a>
           <a href="#">Liên Hệ</a>
         </nav>
         <div className="actions">
-          <button className="login-btn">Đăng Nhập</button>
+          <button className="login-btn" onClick={() => navigate('/authentication')}>Đăng Nhập</button>
           <button className="appointment-btn">Đặt Lịch Hẹn</button>
         </div>
     </header>
