ab8e9aba54cddcf09207e46ef389b20df6e7fe22
Pi2608 <ibaxq26@gmail.com>
2025-06-17 09:49:50 +0700

    PDat - Login Status done

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,14 +1,32 @@
-import React from "react"
+import React, { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
-import { router } from './Router'
+import { useDispatch, useSelector } from 'react-redux';
+import { router } from './Router';
+import { checkAuthStatus } from '@features/auth/authSlice'; // Thay đổi path phù hợp
 import './App.css';
 
 export default function App() {
+  const dispatch = useDispatch();
+  const { loading } = useSelector(state => state.auth);
+
+  useEffect(() => {
+    // Kiểm tra trạng thái đăng nhập khi app khởi động
+    dispatch(checkAuthStatus());
+  }, [dispatch]);
+
+  // Hiển thị loading khi đang kiểm tra trạng thái
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <div>Đang tải...</div>
+        {/* Hoặc component loading spinner của bạn */}
+      </div>
+    );
+  }
 
   return (
     <div>
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
-
