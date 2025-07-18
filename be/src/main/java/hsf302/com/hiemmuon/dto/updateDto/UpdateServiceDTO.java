224318f308344fc555ecdd4755de40f465cff61a
sondx24 <sondxse181792@fpt.edu.vn>
2025-07-18 14:45:15 +0700

    successrate

--- be/src/main/java/hsf302/com/hiemmuon/dto/updateDto/UpdateServiceDTO.java
+++ be/src/main/java/hsf302/com/hiemmuon/dto/updateDto/UpdateServiceDTO.java
@@ -22,22 +22,22 @@ public class UpdateServiceDTO {
     @Size(max = 1000, message = "Mô tả không được vượt quá 1000 ký tự")
     private String description;
 
+    @Size(max = 2000, message = "Thông tin đối tượng phù hợp không được vượt quá 2000 ký tự")
+    private String targetPatient;
+
     @DecimalMin(value = "0.0", inclusive = true, message = "Tỷ lệ thành công phải từ 0% trở lên")
     @DecimalMax(value = "100.0", inclusive = true, message = "Tỷ lệ thành công không được vượt quá 100%")
     private Float successRate;
+
+    @NotBlank(message = "Lợi ích không được để trống")
+    private String benefit;
+
+    @Size(max = 5000, message = "FAQ không được vượt quá 5000 ký tự")
+    private String faq;
 
     @DecimalMin(value = "0.0", inclusive = false, message = "Giá tiền phải lớn hơn 0")
     private BigDecimal price;
 
     @Size(max = 1000, message = "Thông số kỹ thuật không được vượt quá 1000 ký tự")
     private String specifications;
-
-    @NotBlank(message = "Lợi ích không được để trống")
-    private String benefit;
-
-    @Size(max = 2000, message = "Thông tin đối tượng phù hợp không được vượt quá 2000 ký tự")
-    private String targetPatient;
-
-    @Size(max = 5000, message = "FAQ không được vượt quá 5000 ký tự")
-    private String faq;
 }
